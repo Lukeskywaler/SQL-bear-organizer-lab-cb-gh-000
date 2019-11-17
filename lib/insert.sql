79752c7588eda1f0560daed5ad7669e8ee054cb2
Vicknesh Jegathesan <e0300583@u.nus.edu>
2019-11-17 15:07:10 +0000

    Automatically backed up by Learn

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (id, name, age, gender,color,temperament,alive) VALUES (1, "Mr. Chocolate", 2, "male", "blue", true);
+INSERT INTO bears (id, name, age, gender,color,temperament,alive) VALUES (1, "Rowdy", 2, "male", "blue", true);
+INSERT INTO bears (id, name, age, gender,color,temperament,alive) VALUES (1, "Tabitha", 2, "female", "blue", true);
+INSERT INTO bears (id, name, age, gender,color,temperament,alive) VALUES (1, "Sergeant Brown", 2, "male", "blue", true);
+INSERT INTO bears (id, name, age, gender,color,temperament,alive) VALUES (1, "Melissa", 2, "female", "blue", true);
+INSERT INTO bears (id, name, age, gender,color,temperament,alive) VALUES (1, "Grinch", 2, "male", "blue", true);
+INSERT INTO bears (id, name, age, gender,color,temperament,alive) VALUES (1, "Wendy", 2, "female", "blue", true);
+INSERT INTO bears (id, name, age, gender,color,temperament,alive) VALUES (1, null, 2, "male", "blue", true);
